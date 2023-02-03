be3785d9443fbf207374f20d2ddd7974e57584cc
allexxlekk <allexxlekk@gmail.com>
2023-02-03 02:36:30 +0200

    Create model with embedded layer

--- neural network files/neuralnet.py
+++ neural network files/neuralnet.py
@@ -3,13 +3,12 @@ import pandas as pd
 from keras.preprocessing.text import one_hot
 from keras.utils import pad_sequences
 from keras.utils import to_categorical
-# TODO Create embedding layer
-#   - Keras
-#   - One hot encoding from vocabulary
-#   - Model
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers import Embedding
 
-# TODO Create output vector
-#   - One hot encoding
+
 
 
 # Word Embedding Parameters
@@ -18,15 +17,33 @@ OUTPUT_DIM = 100    # Size of the output vectors
 INPUT_LENGTH = 50   # Size of largest input text
 
 ratings = []
-df_data = pd.read_csv('temp.csv')
+summaries = []
+df_data = pd.read_csv('train.csv')
 for i,row in df_data.iterrows():
     ratings.append(int(row['rating']))
-    
+    summaries.append(row['summary'])
 
-# Example of movie ratings (1 to 10)
-labels = np.array(ratings)
 
-# One-hot encoding of the ratings
-one_hot_ratings = to_categorical(ratings - 1, num_classes=10)
+# Encode summaries for embedding
+encoded_summaries = [one_hot(d, INPUT_DIM) for d in summaries]
+print(encoded_summaries)
+padded_summaries = pad_sequences(encoded_summaries, maxlen=INPUT_LENGTH, padding='post')
 
-print(one_hot_ratings)
+# Create One Hot Encoding For Rating
+ratings = np.array(ratings)
+labels = to_categorical(ratings - 1, num_classes=10)
+
+# define the model
+model = Sequential()
+model.add(Embedding(INPUT_DIM, 8, input_length=INPUT_LENGTH, name="embeddings"))
+model.add(Flatten())
+model.add(Dense(10, activation='softmax'))
+# compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+# summarize the model
+print(model.summary())
+# fit the model
+model.fit(padded_summaries, labels, epochs=50, verbose=1)
+# evaluate the model
+loss, accuracy = model.evaluate(padded_summaries, labels, verbose=1)
+print('Accuracy: %f' % (accuracy*100))
